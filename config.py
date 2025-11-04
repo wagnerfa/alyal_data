8725c38579eb2c03861be2e44bfae57013d65e3c
WagnerAndrade <wagnerandrade.dev@gmail.com>
2025-11-04 11:08:45 -0300

    Revert "Primeiro Commit"
    This reverts commit ee6e86a7c7a40c31625c14feb770ce87fb036287.

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'sua-chave-secreta-super-segura-aqui'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
